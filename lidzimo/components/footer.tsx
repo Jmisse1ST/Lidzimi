b6e7680b1d10ce5e435d15b9951cb899cfb0f222
Jemisse <macuacuajemisse@gmail.com>
2025-11-12 13:21:12 +0200

    UPDATE: Adjust footer styling and content for improved readability

--- lidzimo/components/footer.tsx
+++ lidzimo/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <section className="py-3 mt-5">
+    <section className="py-3 mt-7">
       <div className="flex flex-col justify-center items-center max-w-7xl mx-auto px-4">
         {/* Linha divisória com 20% de margem em cada lado */}
         <div className="w-full flex justify-center items-center mb-3">
@@ -11,9 +11,10 @@ const Footer = () => {
         
         {/* Conteúdo centralizado */}
         <div className="flex flex-col items-center text-center">
-          <p className="text-sm text-gray-800 py-3">Mantido por: Jemisse Castigo Macuacua</p>
+          <p className="text-sm text-gray-500  font-mono">Mantido por: Jemisse Macuacua </p>
+          <p className="text-sm text-gray-500  font-mono"> Software Developer </p>
          
-          <span className="text-xs text-gray-500 mt-1 ">v1.0.0</span>
+          <span className="text-xs text-gray-500 mt-6 font-mono">v1.0.0</span>
         </div>
       </div>
     </section>
